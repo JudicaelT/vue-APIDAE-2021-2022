c1c4444b8559bea4b4a143507ee9b183d39b7538
Judicaël Terrisse <judicael.terrisse@etu.univ-amu.fr>
2022-02-14 10:18:04 +0100

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header">
+    <router-link to="/">Accueil</router-link>
+    <router-link to="recettes">Recettes</router-link>
+    <router-link to="login">Connexion</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+#header {
+  min-height: 80px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>This is some content</p>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+</style>
