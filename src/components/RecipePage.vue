46a2e9a0dc5cbc938e88f9c5c63b291005691062
Judicael <judicael.terrisse@etu.umontpellier.fr>
2022-03-11 00:06:00 +0100

    Séparation de RecipePage en components + props pour RecipePage

--- src/components/RecipePage.vue
+++ src/components/RecipePage.vue
@@ -1,155 +1,76 @@
 <template>
   <div class="container">
 
-    <div class="row">
-      <div class="col-8">
-        <div class="row">
-          <div class="col">
-            <h1>Cassoulet à ma façon</h1>
-            <router-link class="btn btn-like shadow" :to="`/ajouter_favori/`">
-              <b-icon-heart-fill class="text-astra"></b-icon-heart-fill>
-              <i class="ml-2">65 j'aimes</i>
-            </router-link>
-          </div>
-        </div>
-        <div class="row mt-4">
-          <div class="col">
-            <i class="text-secondary">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione fugit facilis assumenda
-              asperiores laborum repudiandae a impedit voluptate numquam neque, dolore voluptatibus,
-              expedita quidem veritatis quaerat illo ab cum beatae!
-            </i>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="col">
-        <b-img class="img-fluid rounded-rounder" :src="getImageUrl('cassoulet.webp')"></b-img>
-      </div>
-    </div>
+    <Heading
+      :id="RecipeData.id"
+      :name="RecipeData.name"
+      :thumbnail="RecipeData.thumbnail"
+      :nbLike="RecipeData.nbLike"
+      :description="RecipeData.description"
+    >
+    </Heading>
 
     <div class="row my-5">
+      <Preparation
+        :preparation="RecipeData.preparation"
+      >
+      </Preparation>
 
-      <div class="col-8">
-        <div class="stepper d-flex flex-column mx-2">
-          <div class="d-flex">
-            <div class="d-flex flex-column pr-4 align-items-center">
-              <div class="rounded-circle py-2 px-3 bg-astra">1</div>
-              <div class="line h-100"></div>
-            </div>
-            <div>
-              <h5 class="mt-2">Etape 1</h5>
-              <p class="lead text-muted pb-3">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut omnis, id facilis tempore
-                possimus repudiandae ex necessitatibus aut soluta sit repellat facere delectus consequatur 
-                ratione nostrum. Facilis vel voluptatem sapiente!
-              </p>
-            </div>
-          </div>
-          <div class="d-flex">
-            <div class="d-flex flex-column pr-4 align-items-center">
-              <div class="rounded-circle py-2 px-3 bg-astra">2</div>
-              <div class="line h-100"></div>
-            </div>
-            <div>
-              <h5 class="mt-2">Etape 2</h5>
-              <p class="lead text-muted pb-3">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut omnis, id facilis tempore
-                possimus repudiandae ex necessitatibus aut soluta sit repellat facere delectus consequatur 
-                ratione nostrum. Facilis vel voluptatem sapiente!
-              </p>
-            </div>
-          </div>
-          <div class="d-flex">
-            <div class="d-flex flex-column pr-4 align-items-center">
-              <div class="rounded-circle py-2 px-3 bg-astra">3</div>
-              <div class="line h-100"></div>
-            </div>
-            <div>
-              <h5 class="mt-2">Etape 3</h5>
-              <p class="lead text-muted pb-3">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut omnis, id facilis tempore
-                possimus repudiandae ex necessitatibus aut soluta sit repellat facere delectus consequatur 
-                ratione nostrum. Facilis vel voluptatem sapiente!
-              </p>
-            </div>
-          </div>
-          <div class="d-flex">
-            <div class="d-flex flex-column pr-4 align-items-center">
-              <div class="rounded-circle py-2 px-3 bg-astra">4</div>
-              <div class="line h-100"></div>
-            </div>
-            <div>
-              <h5 class="mt-2">Etape 4</h5>
-              <p class="lead text-muted pb-3">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut omnis, id facilis tempore
-                possimus repudiandae ex necessitatibus aut soluta sit repellat facere delectus consequatur 
-                ratione nostrum. Facilis vel voluptatem sapiente!
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col h-100 rounded-rounder bg-astra p-3">
-        <h5>Informations</h5>
-        <ul class="list-unstyled pl-3">
-          <li>
-            <b-icon-people></b-icon-people>
-            <label class="px-2">Quantité :</label>
-            <b>1 personne</b>
-          </li>
-          <li>
-            <b-icon-clock></b-icon-clock>
-            <label class="px-2">Temps :</label>
-            <b>20 minutes</b>
-          </li>
-          <li>
-            <b-icon-info-circle></b-icon-info-circle>
-            <label class="px-2">Niveau : </label>
-            <b>Expert</b>
-          </li>
-        </ul>
-        
-        <h5>Ingrédients</h5>
-        <ul>
-          <li>
-            Patates <small class="text-secondary">200g</small>
-          </li>
-          <li>
-            carottes <small class="text-secondary">75g</small>
-          </li>
-          <li>
-            Eau <small class="text-secondary">100ml</small>
-          </li>
-        </ul>
-      </div>
-
+      <Informations
+        :informations="RecipeData.informations"
+        :ingredients="RecipeData.ingredients"
+      >
+      </Informations>
     </div>
 
   </div>
 </template>
   
 <script>
+  import Heading from "@/components/RecipePage/Heading";
+  import Preparation from "@/components/RecipePage/Preparation";
+  import Informations from "@/components/RecipePage/Informations";
+
   export default {
     name: "RecipePage",
-    mounted(){
-      this.setThumbnailHeight(1); // 100%
+    components: {
+      Heading,
+      Preparation,
+      Informations
     },
+    data () {
+			return {
+				RecipeData: {
+          id: 1,
+          name: 'Recette1',
+          thumbnail: 'cassoulet.webp',
+          nbLike: 65,
+          description: 'Lorem',
+          preparation: 'Lorem Ipsum',
+          informations: {
+            quantity: 5,
+            time: 20,
+            level: 'amateur',
+          },
+          ingredients: [
+            {
+              name: 'patates',
+              quantity: 1,
+              typeQuantity: 'kg',
+            },
+            {
+              name: 'carrotes',
+              quantity: 500,
+              typeQuantity: 'g',
+            },
+            {
+              name: 'Eau',
+              quantity: 400,
+              typeQuantity: 'ml',
+            },
+          ],
+        },
+			}
+		}
   };
 </script>
-  
-<style scoped>
-
-
-  .line {
-    width: 2px;
-    background-color: var(--grey);
-  }
-
-  .lead {
-    font-size: 1.1rem;
-  }
-
-</style>
---
+++ src/components/RecipePage/Heading.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="row">
+    <div class="col-8">
+      <div class="row">
+        <div class="col">
+          <h1>{{ name }}</h1>
+          <router-link class="btn btn-like shadow" :to="`/ajouter_favori/${id}`">
+            <b-icon-heart-fill class="text-astra"></b-icon-heart-fill>
+            <i class="ml-2">{{ nbLike }} j'aimes</i>
+          </router-link>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col">
+          <i class="text-secondary">
+            {{ description }}
+          </i>
+        </div>
+      </div>
+    </div>
+		<div class="col">
+			<b-img class="img-fluid rounded-rounder" :src="getImageUrl( `${thumbnail}` )"></b-img>
+		</div>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: "Heading",
+    props: [ 'id', 'name', 'thumbnail', 'nbLike', 'description', ],
+  };
+</script>
+
+<style scoped></style>
