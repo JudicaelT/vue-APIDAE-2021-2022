41375aa7013647c20109ca795e8e7b50ea19623d
Judicael <judicael.terrisse@etu.umontpellier.fr>
2022-03-14 00:12:39 +0100

    Ajouter et supprimmer des ingrédients

--- src/components/AddRecipe.vue
+++ src/components/AddRecipe.vue
@@ -25,10 +25,13 @@
 
 					<h2 class="mt-5">Ingr&eacute;dients :</h2>
 					<div class="recipe-lines">
-						<div class="row" id="recipe">
+						<div class="row"
+						v-for="ingredient in ingredients"
+							:id="ingredient.id"
+							:key="ingredient.key">
 							<div class="col-md-6">
 								<div>
-									<label class="form-label mt-3" for="ingredientName">Nom :</label>
+									<label class="form-label mt-3" for="ingredientName"><b-icon-x-circle v-on:click="removeIngredient(ingredient.id)" class="text-danger cursor-pointer"></b-icon-x-circle> Nom :</label>
 									<input class="form-control rounded py-4 border-0 bg-grey" type="text" name="ingredientName" />
 								</div>
 							</div>
@@ -44,39 +47,12 @@
 								<div>
 									<label class="form-label mt-3" for="quantityType">Type :</label>
 									<input class="form-control rounded py-4 border-0 bg-grey" type="text"
-										placeholder="ex : kg, verre, ..." name="quantityType" />
-								</div>
-							</div>
-						</div>
-
-						<div class="row" id="recipe-2">
-							<div class="col">
-								<div>
-									<label class="form-label mt-3" for="ingredientName">
-										<b-icon-x-circle v-on:click="removeIngredient(2)" class="text-danger cursor-pointer">
-										</b-icon-x-circle> Nom :
-									</label>
-									<input class="form-control rounded py-4 border-0 bg-grey" type="text" name="ingredientName" />
-								</div>
-							</div>
-
-							<div class="col-md-3">
-								<div>
-									<label class="form-label mt-3" for="ingredientQuantity">Quantit&eacute; :</label>
-									<input class="form-control rounded py-4 border-0 bg-grey" type="number" name="ingredientQuantity" />
-								</div>
-							</div>
-
-							<div class="col-md-3">
-								<div>
-									<label class="form-label mt-3" for="quantityType">Type :</label>
-									<input class="form-control rounded py-4 border-0 bg-grey" type="text"
-										placeholder="ex : kg, verre, ..." name="quantityType" />
+										placeholder="ex : kg" name="quantityType" />
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="btn btn-outline-dark rounded py-2 my-4" v-on:click="addIngredient()">
+					<div class="btn btn-outline-dark rounded py-2 my-4" v-on:click="addIngredient(count)">
 						<b-icon-plus-circle></b-icon-plus-circle>
 						Ajouter un autre ingrédient
 					</div>
@@ -93,22 +69,47 @@
 		data: function () {
 			return {
 				count: 0,
+				ingredients: [{
+					id: 0
+				}],
 			}
 		},
 		methods: {
 
+			/**
+			 * Add a section with inputs fields to fill an ingredient.
+			 * The section is created with an id so that it can be removed
+			 * with 'removeIngredient(id)' method
+			 */
 			addIngredient: function () {
-				let id = this.count++;
-
-				let newIngredient = document.getElementById('recipe').cloneNode(true);
-				
-				newIngredient.id = 'recipe-' + id;
-				newIngredient.querySelector('label[for="ingredientName"]').innerHTML = '<b-icon-x-circle v-on:click="removeIngredient(' + id + ')" class="text-danger cursor-pointer"></b-icon-x-circle> Nom :';
-
+				this.ingredients.push({
+					id: ++this.count,
+				});
 			},
 
+			/**
+			 * Remove a section to add an ingredient. The section will
+			 * not be removed if it is the only section in the form
+			 * (so that the recipe has at least 1 ingredient)
+			 * 
+			 * @param id the id attribute of the section we want to remove
+			 */
 			removeIngredient: function (id) {
-				document.getElementById('recipe-' + id).remove();
+				if(this.ingredients.length <= 1) {
+					alert('La recette doit avoir au moins 1 ingrédient');
+					return;
+				}
+
+				let ingredientIndex = this.ingredients.findIndex(ingredient => {
+					return ingredient.id === id;
+				});
+
+				console.log(ingredientIndex);
+				if (ingredientIndex !== -1) {
+					this.ingredients.splice(ingredientIndex, 1);
+				} else {
+					console.log('toto');
+				}
 			},
 
 			addRecipe: function () {
@@ -118,6 +119,3 @@
 
 	};
 </script>
-
-<style scoped>
-</style>
