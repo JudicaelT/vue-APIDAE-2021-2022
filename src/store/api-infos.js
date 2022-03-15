e9197c40a228f6679dbf693c965d67651ced26ae
Joachim <60385078+joachim-dasilva@users.noreply.github.com>
2022-03-15 17:59:37 +0100

    fix deploy bug

--- src/store/api-infos.js
+++ src/store/api-infos.js
@@ -2,6 +2,7 @@ export const API_URL = "https://dsv-api-recipe.herokuapp.com/";
 
 export function authHeader() {
   const user = JSON.parse(localStorage.getItem("user"));
-  if (user?.tokenJWT) return { Authorization: "Bearer " + user.tokenJWT };
+  if (user && user.tokenJWT)
+    return { Authorization: "Bearer " + user.tokenJWT };
   return {};
 }
